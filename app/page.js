1e843e8c8d2f67c9f3c0c47b683dd8cbb3411753
Arthur Birck Lopes <arthur.bircklopes@edu.sait.ca>
2024-06-04 14:30:30 -0600

    assignment 4

--- app/page.js
+++ app/page.js
@@ -45,12 +45,19 @@ export default function Home() {
         </a>
 
         
+        <a
+          href="/week-4"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Week-4{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
+        </a>
         
 
         
